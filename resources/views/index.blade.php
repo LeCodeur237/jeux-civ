f5ee0b4ff5b8950742a065a3ab088fb4cb3b53ed
LeCodeur237 <erastedzou@gmail.com>
2026-02-10 18:34:30 +0100

    Mise à jour des configurations de l'environnement et du titre de la page d'accueil

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Grand Jeu Elles Plus Africa | By DigiTexia</title>
+    <title>Jeu instant gagnant Special St Valentin | By Diversitypub</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
